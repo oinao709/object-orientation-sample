9915cf1cb0c3d8d52c5ec5839d16f14ad30a933a
oinao709 <oinao777@gmail.com>
2023-08-18 08:59:34 +0900

    お題完成

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("種別：いぬ 鳴き声：わんわん");
+        System.out.println("種別：ねこ 鳴き声：にゃーにゃー");
     }
 }
